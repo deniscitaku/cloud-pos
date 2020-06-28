852c23a0cc39582580848b9aa4e352ff163c17ef
Denis Citaku <denis.citaku@gmail.com>
2020-06-28 18:06:54 +0200

    Initial commit

---
+++ gateway/src/main/java/com/denlir/gateway/GatewayApplication.java
@@ -0,0 +1,25 @@
+package com.denlir.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("pos", r -> r.path("/pos/**").uri("lb://pos"))
+				.build();
+	}
+
+}
---
+++ service-discovery/src/test/java/com/denlir/gateway/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denlir.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
